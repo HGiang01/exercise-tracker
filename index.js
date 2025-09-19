4066b3d797da24ee6760e5fdd8a4d468a1f6aaef
HGiang01 <nguyenhoanggiang090804@gmail.com>
2025-09-19 22:36:58 +0700

    refactor: convert to ESModule format and change static router /  to /public

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+// Import required modules
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import express from "express";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 
-app.use(cors())
-app.use(express.static('public'))
+// Configure and init app
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+// Set up middlewares
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use("/public", express.static(path.join(process.cwd(), "/public")));
+
+// Define routers
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
+// Start server
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
